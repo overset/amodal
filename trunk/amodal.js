950f6b0fca16a2b4132dd56a631d3fbc74092506
jimpalmer <jimpalmer@af2d09bd-c3db-bc0b-86cd-5ec81ac79fee>
2010-10-29 23:50:07 +0000

    v0.2: numerous additions to handle exceptions with position of modals, leak with unbound keyhandler, handles window resizing somewhat gracefully, now overload the per-modal CSS if sending object through amodal plugin function

--- trunk/amodal.js
+++ trunk/amodal.js
@@ -1,23 +1,27 @@
 /**
- * amodal.js - v0.1 - ANOTHER modal - jquery plugin - MIT license
+ * amodal.js - v0.2 - ANOTHER modal - jquery plugin - MIT license
  * Author: Jim Palmer
  */
 (function ($) {
 	$.fn.amodal = function (options) {
 		var close = function (m) {
 			$(['.amodal','.amodalborder','.amodalmask'].join(m + ',') + m).remove();
+			if (!$('.amodal').length) {
+				$(document).unbind('keydown.amodal')
+				$(window).unbind('resize.amodal');
+			}
 		};
 		// if no options arg passed - destroy newest amodal (FILO)
 		if (typeof options == 'undefined') {
 			var amodal = this.siblings('.amodal')[0] || {},
 				ami = (amodal.className || '').replace(/[^0-9]/g, '');
-			if (ami)
-				close(ami);
+			ami && close(ami);
 			return this;
 		}
 		var opts = $.extend({
 				mask: true,			// mask target area
 				maskAll: false,		// mask entire document
+				windowResize: true,	// resize modal on window.onResize
 				defOpacity: 0.6,	// default mask and border opacity
 				escCancel: true,	// ESC key closes modal
 				zStart: 1300,		// beginning z-index value for modal
@@ -29,57 +33,73 @@
 			message = typeof options == 'string' ? options : options.message || '',
 			divMarkup = function (c, m, v) {
 				return '<div class="' + c + ' ' + c + m + '">' + (v || '') + '</div>';
+			},
+			reposition = function (target, m) {
+				var selSuffix = typeof m != 'undefined' ? m : '',
+					amodal = target.siblings('.amodal' + selSuffix);
+				amodal.css({
+						top: Math.max(opts.borderPad,
+							target.offset().top + 
+								(target.outerHeight(1) / 2) - 
+								($(this).outerHeight(1) / 2)),
+						left: Math.max(opts.borderPad, Math.min(
+							$(window).width() - $(this).outerWidth(1) - opts.borderPad,
+							target.offset().left + 
+								(target.outerWidth(1) - $(this).outerWidth(1)) / 2))
+					}).end()
+					.siblings('.amodalborder' + selSuffix).css({
+						left: (parseInt(amodal.css('left')) || 0 ) - opts.borderPad,
+						top: (parseInt(amodal.css('top')) || 0 ) - opts.borderPad,
+						height: amodal.height(),
+						width: amodal.width()
+					}).end()
+					.siblings('.amodalmask' + selSuffix).css($.extend(target.offset(), {
+						width: target.outerWidth(1),
+						height: target.outerHeight(1)
+					}));
 			};
+		if (opts.windowResize)
+			$(window).unbind('resize.amodal').bind('resize.amodal', function () {
+				$(body).children('.amodalmask')
+					.css({width: $(window).width(), height:$(document).height()});
+				if (!$(document).data('amodal.resizeTO'))
+					$(document).data('amodal.resizeTO', setTimeout(function () {
+						$('.amodal').each(function () {
+							reposition.call(this, $(this).data('target'));
+						});
+						$(document).data('amodal.resizeTO', null)
+					}, 5));
+			});
 		if (opts.escCancel)
 			$(document).unbind('keydown.amodal').bind('keydown.amodal', function (e) {
 				if (e.keyCode == 27 || e.charCode == 27)
 					close($('.amodal').length);
-				!$('.amodal').length && $(this).unbind('keydown.amodal');
 			});
 		return this.each(function () {
-			var target = $(this).length ? $(this) : body,
+			var target = $(this).length ? $(this) : $(body),
 				z = opts.zStart + (mIndex * 3) + 300,
 				m = ++mIndex,
-				maskCSS = {
-					width:$(window).width(),
-					height:$(window).height(),
-					opacity:opts.defOpacity,
-					zIndex:opts.zStart
-				};
+				maskCSS = {opacity: opts.defOpacity, zIndex: opts.zStart};
 			if (opts.maskAll)
 				body.append(divMarkup('amodalmask', m))
-					.find('.amodalmask' + m).css(maskCSS).end()
+					.find('.amodalmask' + m).css(maskCSS).css({
+						width: $(window).width(), height:$(document).height()
+					}).end()
 			target.before(divMarkup('amodal', m) + divMarkup('amodalborder', m) +
 					(opts.mask && !opts.maskAll ? divMarkup('amodalmask', m) : ''))
 				.siblings('.amodal' + m)
 					.html((opts.close ? divMarkup('amodalclose', m, opts.close) : '') + message)
-					.each(function () {
-						$(this).css({
-							top:Math.max(10, target.offset().top - ($(this).height() / 2)),
-							left:target.offset().left + 
-								(target.outerWidth(1) - $(this).outerWidth(1)) / 2,
-							zIndex:z--
-						});
-					})
+					.css(opts.css || {}).css({display:'block', zIndex:z--})
+					.data('target', target)
 					.find('.amodalclose').click(function () { 
 						target.amodal();
 					}).end()
+					.each(function () {
+						reposition.call(this, target, m);
+					})
 				.end()
-				.siblings('.amodalborder' + m).each(function () {
-					var p = $(this).siblings('.amodal' + m);
-					$(this).css({
-						left:(parseInt(p.css('left')) || 0 ) - opts.borderPad, 
-						top:(parseInt(p.css('top')) || 0 ) - opts.borderPad, 
-						height:p.height(), 
-						width:p.width(), 
-						opacity:opts.defOpacity, 
-						zIndex:z--
-					});
-				}).end()
-				.siblings('.amodalmask' + m).css($.extend(maskCSS, target.offset(), {
-					width:target.outerWidth(1),
-					height:target.outerHeight(1)
-				})).end();
+				.siblings('.amodalborder' + m).css({opacity: opts.defOpacity, zIndex:z--}).end()
+				.siblings('.amodalmask' + m).css(maskCSS);
 		});
 	};
 })(jQuery);
